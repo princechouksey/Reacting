7e93fb3898ee512238aa2f366baabeddab064da2
Prince Chouksey <Princechouksey179@gmail.com>
2024-12-20 22:50:53 +0530

    done

--- promises/src/App.jsx
+++ promises/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useSyncExternalStore } from 'react';
 import axios from 'axios';
 
 const App = () => {
@@ -6,6 +6,7 @@ const App = () => {
   const [Company, setCompany] = useState('');
   const [Number, setNumber] = useState('');
   const [allUsers, setAllUsers] = useState([]);
+  const [favourite, setFavourite] = useState(false);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -15,6 +16,7 @@ const App = () => {
       name: Name,
       company: Company,
       phone: Number,
+      favourite: favourite,
     };
 
     // Update the allUsers state with the new user
@@ -24,6 +26,7 @@ const App = () => {
     setName('');
     setCompany('');
     setNumber('');
+    setFavourite(false)
   };
   const deleteHandler = (i)=>{
     const newArr = [...allUsers]
@@ -54,14 +57,16 @@ const App = () => {
           />
           <h1 className="text-xl font-sans mb-1">Phone</h1>
           <input
-            onChange={(e) => setNumber(e.target.value)}
+             
             value={Number}
             className="border border-black px-2 py-1 rounded mb-5 w-full"
             type="text"
             placeholder="Enter Phone Number"
           />
           <br />
-          <button className="px-10 py-2 rounded bg-blue-600 text-white w-full">Add Contact</button>
+          <input onChange={(e) => setFavourite(e.target.checked)}  type="checkBox" checked={favourite}  />
+          <label htmlFor="checkBox" className='ml-3 text-xl mb-5'>Favourite</label>
+          <button className="px-10 py-2 rounded bg-blue-600 text-white w-full mt-4">Add Contact</button>
         </form>
       </div>
       <div className="w-[50%] h-full bg-gray-100 px-5 py-2">
@@ -71,8 +76,12 @@ const App = () => {
             <h1 className="text-2xl font-semibold">{user.name}</h1>
             <h2 className="text-xl">Company: {user.company}</h2>
             <h2 className="text-xl">Phone: {user.phone}</h2>
+            <div className='flex items-center justify-between'>
+            <h2 className="text-xl w-fit px-3 py-2 rounded  bg-yellow-500 text-white" >{user.favourite ? "Favourite" : "" }</h2>
             <button onClick={()=>{deleteHandler(index)}} className='px-3 py-1 rounded bg-red-600 text-white text-lg mt-5 '>Delete</button>
-          </div>
+         
+            </div>
+            </div>
         ))}
       </div>
     </div>
